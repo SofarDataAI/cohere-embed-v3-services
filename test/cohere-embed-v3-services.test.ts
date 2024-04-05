571940feaa1654b248482b4048cb3fad864efc8e
KennyDizi <xamarindevelopervietnam@outlook.com>
2024-04-05 20:54:46 +0700

    Init project CDK

---
+++ test/cohere-embed-v3-services.test.ts
@@ -0,0 +1,17 @@
+// import * as cdk from 'aws-cdk-lib';
+// import { Template } from 'aws-cdk-lib/assertions';
+// import * as CohereEmbedV3Services from '../lib/cohere-embed-v3-services-stack';
+
+// example test. To run these tests, uncomment this file along with the
+// example resource in lib/cohere-embed-v3-services-stack.ts
+test('SQS Queue Created', () => {
+//   const app = new cdk.App();
+//     // WHEN
+//   const stack = new CohereEmbedV3Services.CohereEmbedV3ServicesStack(app, 'MyTestStack');
+//     // THEN
+//   const template = Template.fromStack(stack);
+
+//   template.hasResourceProperties('AWS::SQS::Queue', {
+//     VisibilityTimeout: 300
+//   });
+});
