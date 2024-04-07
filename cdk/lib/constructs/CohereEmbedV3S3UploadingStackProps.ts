6328f07ee1a5da52813fe0ba1b3e0e3f3893f29e
KennyDizi <xamarindevelopervietnam@outlook.com>
2024-04-07 15:39:56 +0700

    Removed unused constructs

--- cdk/lib/constructs/CohereEmbedV3S3UploadingStackProps.ts
+++
@@ -1,9 +0,0 @@
-import * as cdk from "aws-cdk-lib";
-import { NestedStackProps, RemovalPolicy } from "aws-cdk-lib";
-import { CohereEmbedV3Services } from "../CohereEmbedV3ServicesStackProps";
-
-export interface CohereEmbedV3S3UploadingStackProps extends CohereEmbedV3Services, NestedStackProps {
-    readonly removalPolicy: RemovalPolicy;
-    readonly uploadingS3Bucket: cdk.aws_s3.Bucket;
-    readonly DATA_INGESTION_API_KEY: string;
-}
