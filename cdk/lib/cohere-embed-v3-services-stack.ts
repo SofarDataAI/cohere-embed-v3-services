e169fba988b61686ca07c15c24ad722dc095b2d4
KennyDizi <xamarindevelopervietnam@outlook.com>
2024-04-07 16:27:09 +0700

    Add CohereEmbedV3ServicesStack document

--- cdk/lib/cohere-embed-v3-services-stack.ts
+++ cdk/lib/cohere-embed-v3-services-stack.ts
@@ -5,7 +5,17 @@ import { Construct } from 'constructs';
 import { CohereEmbedV3ServicesStackProps } from './CohereEmbedV3ServicesStackProps';
 import { PythonFunction } from '@aws-cdk/aws-lambda-python-alpha';
 
+/**
+ * The `CohereEmbedV3ServicesStack` class defines the AWS infrastructure as code for the Cohere Embed V3 Services.
+ * It sets up a Lambda function configured to use the Cohere API for natural language processing tasks.
+ */
 export class CohereEmbedV3ServicesStack extends cdk.Stack {
+  /**
+   * Constructs a new instance of the CohereEmbedV3ServicesStack.
+   * @param scope The scope in which to define this construct. Usually an `App` or a `Stage`.
+   * @param id A unique identifier for the stack.
+   * @param props The stack properties, including the Cohere API key, model, and other AWS resource configurations.
+   */
   constructor(scope: Construct, id: string, props: CohereEmbedV3ServicesStackProps) {
     super(scope, id, props);
 
